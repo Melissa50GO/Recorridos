ade226c8f5c612896cdfdc34d873fb4befbd9f98
Melissa Stephanie Alejandra Guillen Olivera <N00383798@upn.pe>
2024-11-26 09:32:14 -0500

    matriz de Floyd

---
+++ Floyd_Warshall.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullPractica
+{
+    internal class Floyd_Warshall
+    {
+        int[,] matrizFinal;
+
+        public void ImprimirMatriz(int[,] matriz)
+        {
+            for(int i=0; i<matriz.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for(int j= 0; j< matriz.GetLength(0); j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+            }
+        }
+
+        public void Floyd(int[,] matriz)
+        {
+            matrizFinal = matriz;
+
+            for(int k=0; k<matriz.GetLength(0); k++)
+            {
+                for(int i=0; i < matriz.GetLength(0); i++)
+                {
+                    for(int j= 0; j < matriz.GetLength(0); j++)
+                    {
+                        if (matrizFinal[i,k] + matrizFinal[k, j] < matrizFinal[i, j])
+                        {
+                            matrizFinal[i, j] = matrizFinal[i, k] + matrizFinal[k, j];
+                        }
+                    }
+                }
+            }
+
+            ImprimirMatriz(matrizFinal);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,54 +9,73 @@ namespace FullPractica
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Grafo g = new Grafo(11);
+        {   //RECORRIDOS    
+            //Grafo g = new Grafo(11);
 
-            g.ConexionAristas(0,1);
-            g.ConexionAristas(0,6);
-            g.ConexionAristas(0,7);
-            g.ConexionAristas(0,8);
-            g.ConexionAristas(0,9);
-            g.ConexionAristas(0,10);
-            g.ConexionAristas(1,0);
-            g.ConexionAristas(1,2);
-            g.ConexionAristas(1,4);
-            g.ConexionAristas(1,10);
-            g.ConexionAristas(2,1);
-            g.ConexionAristas(2,3);
-            g.ConexionAristas(2,10);
-            g.ConexionAristas(3,2);
-            g.ConexionAristas(3,4);
-            g.ConexionAristas(3,5);
-            g.ConexionAristas(4,1);
-            g.ConexionAristas(4,3);
-            g.ConexionAristas(4,5);
-            g.ConexionAristas(4,6);
-            g.ConexionAristas(5,3);
-            g.ConexionAristas(5,4);
-            g.ConexionAristas(5,6);
-            g.ConexionAristas(6,0);
-            g.ConexionAristas(6,0);
-            g.ConexionAristas(6,4);
-            g.ConexionAristas(6,5);
-            g.ConexionAristas(6,7);
-            g.ConexionAristas(7,0);
-            g.ConexionAristas(7,6);
-            g.ConexionAristas(7,8);
-            g.ConexionAristas(8,0);
-            g.ConexionAristas(8,7);
-            g.ConexionAristas(8,9);
-            g.ConexionAristas(9,0);
-            g.ConexionAristas(9,8);
-            g.ConexionAristas(9,10);
-            g.ConexionAristas(10,0);
-            g.ConexionAristas(10,1);
-            g.ConexionAristas(10,2);
-            g.ConexionAristas(10,9);
+            //g.ConexionAristas(0,1);
+            //g.ConexionAristas(0,6);
+            //g.ConexionAristas(0,7);
+            //g.ConexionAristas(0,8);
+            //g.ConexionAristas(0,9);
+            //g.ConexionAristas(0,10);
+            //g.ConexionAristas(1,0);
+            //g.ConexionAristas(1,2);
+            //g.ConexionAristas(1,4);
+            //g.ConexionAristas(1,10);
+            //g.ConexionAristas(2,1);
+            //g.ConexionAristas(2,3);
+            //g.ConexionAristas(2,10);
+            //g.ConexionAristas(3,2);
+            //g.ConexionAristas(3,4);
+            //g.ConexionAristas(3,5);
+            //g.ConexionAristas(4,1);
+            //g.ConexionAristas(4,3);
+            //g.ConexionAristas(4,5);
+            //g.ConexionAristas(4,6);
+            //g.ConexionAristas(5,3);
+            //g.ConexionAristas(5,4);
+            //g.ConexionAristas(5,6);
+            //g.ConexionAristas(6,0);
+            //g.ConexionAristas(6,0);
+            //g.ConexionAristas(6,4);
+            //g.ConexionAristas(6,5);
+            //g.ConexionAristas(6,7);
+            //g.ConexionAristas(7,0);
+            //g.ConexionAristas(7,6);
+            //g.ConexionAristas(7,8);
+            //g.ConexionAristas(8,0);
+            //g.ConexionAristas(8,7);
+            //g.ConexionAristas(8,9);
+            //g.ConexionAristas(9,0);
+            //g.ConexionAristas(9,8);
+            //g.ConexionAristas(9,10);
+            //g.ConexionAristas(10,0);
+            //g.ConexionAristas(10,1);
+            //g.ConexionAristas(10,2);
+            //g.ConexionAristas(10,9);
 
-            g.Amplitud(0);
-            Console.WriteLine("\nProfundidad: ");
-            g.Profundidad(0);
+            //g.Amplitud(0);
+            //Console.WriteLine("\nProfundidad: ");
+            //g.Profundidad(0);
+
+            //Dist min entre par de nodos
+            Floyd_Warshall g = new Floyd_Warshall();
+            int inf = 9999;
+
+            int[,] matriz =
+            {
+                {0,4,5,20, inf},
+                {inf,0,inf,10, inf},
+                {inf,inf,0,8, inf},
+                {inf,inf,inf,0, 2},
+                {inf,inf,inf,inf, 0},
+
+            };
+
+            Console.WriteLine("Matriz INICIAL: ");
+            g.ImprimirMatriz(matriz);
+            Console.WriteLine("\nMatriz con las distancias minimas entre pares:");
+            g.Floyd(matriz);
 
             Console.ReadKey();
         }

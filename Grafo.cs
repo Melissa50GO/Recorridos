67bd8228ed1f2ba5c97e4671fe9844e208d77755
Meli <Meli@DESKTOP-73GO3S5>
2024-11-26 05:46:13 -0500

    Agregar archivos de proyecto.

---
+++ Grafo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullPractica
+{
+    internal class Grafo
+    {
+        private LinkedList<int>[] ArregloListAdya;
+        int v;
+
+        public Grafo(int vertices)
+        {
+            v = vertices;
+            ArregloListAdya = new LinkedList<int>[v];
+            for(int i= 0; i<v; i++)
+            {
+                ArregloListAdya[i] = new LinkedList<int>();
+            }
+        }
+
+        public void ConexionAristas(int origen, int destino)
+        {
+            ArregloListAdya[origen].AddLast(destino);
+            ArregloListAdya[destino].AddLast(origen);
+        }
+
+        public void Amplitud(int origen)
+        {
+            int actual;
+            Queue<int> cola = new Queue<int>();
+            bool[] visitados = new bool[v];
+
+            cola.Enqueue(origen);
+            while(cola.Count > 0)
+            {
+                actual = cola.Dequeue();
+                visitados[actual] = true;
+                Console.Write(actual + " ");
+                foreach(int n in ArregloListAdya[actual])
+                {
+                    if (!visitados[n])
+                    {
+                        cola.Enqueue(n);
+                        visitados[n] = true;
+                    }
+                }
+            }
+
+        }
+
+        private void Profundidad(int origen, bool[] visitados)
+        {
+            visitados[origen] = true;
+            Console.Write(origen + " ");
+
+            foreach(int n in ArregloListAdya[origen])
+            {
+                if (!visitados[n])
+                {
+                    Profundidad(n, visitados);
+                }
+            }
+        }
+
+        public void Profundidad(int origen)
+        {
+            bool[] visit = new bool[v];
+            Profundidad(origen, visit);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullPractica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafo g = new Grafo(11);
+
+            g.ConexionAristas(0,1);
+            g.ConexionAristas(0,6);
+            g.ConexionAristas(0,7);
+            g.ConexionAristas(0,8);
+            g.ConexionAristas(0,9);
+            g.ConexionAristas(0,10);
+            g.ConexionAristas(1,0);
+            g.ConexionAristas(1,2);
+            g.ConexionAristas(1,4);
+            g.ConexionAristas(1,10);
+            g.ConexionAristas(2,1);
+            g.ConexionAristas(2,3);
+            g.ConexionAristas(2,10);
+            g.ConexionAristas(3,2);
+            g.ConexionAristas(3,4);
+            g.ConexionAristas(3,5);
+            g.ConexionAristas(4,1);
+            g.ConexionAristas(4,3);
+            g.ConexionAristas(4,5);
+            g.ConexionAristas(4,6);
+            g.ConexionAristas(5,3);
+            g.ConexionAristas(5,4);
+            g.ConexionAristas(5,6);
+            g.ConexionAristas(6,0);
+            g.ConexionAristas(6,0);
+            g.ConexionAristas(6,4);
+            g.ConexionAristas(6,5);
+            g.ConexionAristas(6,7);
+            g.ConexionAristas(7,0);
+            g.ConexionAristas(7,6);
+            g.ConexionAristas(7,8);
+            g.ConexionAristas(8,0);
+            g.ConexionAristas(8,7);
+            g.ConexionAristas(8,9);
+            g.ConexionAristas(9,0);
+            g.ConexionAristas(9,8);
+            g.ConexionAristas(9,10);
+            g.ConexionAristas(10,0);
+            g.ConexionAristas(10,1);
+            g.ConexionAristas(10,2);
+            g.ConexionAristas(10,9);
+
+            g.Amplitud(0);
+            Console.WriteLine("\nProfundidad: ");
+            g.Profundidad(0);
+
+            Console.ReadKey();
+        }
+    }
+}
